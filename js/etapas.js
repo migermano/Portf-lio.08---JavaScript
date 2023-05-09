78f0efcd8eb317bbdde399992fa54ad162f30010
Mirella Germano <mirellagermano0@gmail.com>
2023-05-09 01:38:50 -0300

    Add files via upload

---
+++ js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos:[
+        {
+            numero:'38111',
+            nome: 'John B Routledge',
+            partido: 'Pogues',
+            fotos:[
+            {url:'John B.jpg', legenda:'Vereador', small: false}
+            ]
+        },
+        {
+            numero:'77222',
+            nome: 'ward Cameron ',
+            partido: 'Kook',
+            fotos:[
+            {url:'ward Cameron.jpg', legenda:'Vereador', small: false}
+            ]
+        },
+        ]
+},
+{
+    titulo: 'PREFEITO',
+    numeros:2,
+    candidatos:[
+        {
+            numero: '99',
+            nome: 'JJ Mayban',
+            partido: 'Pogues',
+            vice: 'Kiara Carrera',
+            fotos:[
+                {url: 'jj.jpg', legenda: 'Prefeito'},
+                {url: 'kiara.jpg', legenda: 'Vice-prefeito', small: true}
+            ]
+        },
+        {
+            numero: '84',
+            nome: 'Rafe Cameronn',
+            partido: 'Kook',
+            vice: 'Rose',
+            fotos:[
+                {url: 'Rafe Cameronn.jpg', legenda: 'Prefeito'},
+                {url: 'Rose.jpg', legenda: 'Vice-prefeito', small: true}
+            ]
+        },
+    ]
+}
+];
